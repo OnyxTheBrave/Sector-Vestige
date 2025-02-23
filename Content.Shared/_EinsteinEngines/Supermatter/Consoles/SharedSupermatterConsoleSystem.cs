a4a49215583cabf8e1147216e7f0dbb7e410321e
hivehum <ketchupfaced@gmail.com>
2025-02-23 10:55:44 -0500

    Merge pull request #1748 from mqole/ee-namespace
    migrate 'einsteinengines' namespace to 'ee'

--- Content.Shared/_EinsteinEngines/Supermatter/Consoles/SharedSupermatterConsoleSystem.cs
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Lachryphage (GitHub)
-// SPDX-FileCopyrightText: 2025 V <97265903+formlessnameless@users.noreply.github.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace Content.Shared._EinsteinEngines.Supermatter.Consoles;
-public abstract partial class SharedSupermatterConsoleSystem : EntitySystem
-{ }
