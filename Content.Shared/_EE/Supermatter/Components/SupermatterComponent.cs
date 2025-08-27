f4e4413f8a06a7ea07bc6d3917e65b8574d0502f
Lachryphage <lachri.lachryphage@gmail.com>
2025-08-27 13:04:52 +1000

    trying to fix this missing monitor component error

--- Content.Shared/_EE/Supermatter/Components/SupermatterComponent.cs
+++ Content.Shared/_EE/Supermatter/Components/SupermatterComponent.cs
@@ -8,7 +8,6 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Shared._EE.Supermatter.Monitor;
 using Content.Shared.Atmos;
 using Content.Shared.DeviceLinking;
 using Content.Shared.DoAfter;
