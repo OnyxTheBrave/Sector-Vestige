2185a9b1cac0cf894ac25cd7b1b9adc7f158acb2
OnyxTheBrave <vinjeerik@gmail.com>
2025-10-14 18:23:34 -0500

    Stage 1: Proof of concept

--- Content.Client/RCD/RCDMenuBoundUserInterface.cs
+++ Content.Client/RCD/RCDMenuBoundUserInterface.cs
@@ -24,6 +24,10 @@ public sealed class RCDMenuBoundUserInterface : BoundUserInterface
             ["Airlocks"] = ("rcd-component-airlocks", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/airlocks.png"))),
             ["Electrical"] = ("rcd-component-electrical", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/multicoil.png"))),
             ["Lighting"] = ("rcd-component-lighting", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/lighting.png"))),
+            ["Piping"] = ("rcd-component-piping", new SpriteSpecifier.Texture(new  ResPath("/Textures/_SV/Interface/Radial/RPD/RPDPiping.png"))), //Sector Vestige: Added RPD
+            ["Utility"] = ("rcd-component-utility", new SpriteSpecifier.Texture(new  ResPath("/Textures/_SV/Interface/Radial/RPD/RPDPiping.png"))), //Sector Vestige: Added RPD
+            ["Vents"] = ("rcd-component-vents", new SpriteSpecifier.Texture(new  ResPath("/Textures/_SV/Interface/Radial/RPD/RPDPiping.png"))), //Sector Vestige: Added RPD
+            ["Pumps"] = ("rcd-component-pumps", new SpriteSpecifier.Texture(new  ResPath("/Textures/_SV/Interface/Radial/RPD/RPDPiping.png"))), //Sector Vestige: Added RPD
         };
 
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
